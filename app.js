aa7690b22eab9d6093370f29b39bd9b9078fd6f5
gulab <mdg122112@gmail.com>
2026-07-01 15:57:11 +0530

    Update project

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
+// const express = require("express");
+
+// const app = express();
+// const PORT = process.env.PORT || 3000;
+
+// app.get("/", (req, res) => {
+//   res.send("Server Running");
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
 const express = require("express");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Static files
+app.use(express.static(path.join(__dirname, "public")));
+
+// Home page
 app.get("/", (req, res) => {
-  res.send("Server Running");
+    res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
+// Server
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`🚀 Server running at http://localhost:${PORT}`);
 });
