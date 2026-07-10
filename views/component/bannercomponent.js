fcef0db14671f2edcea3c638d857502fb83fb392
gulab <mdg122112@gmail.com>
2026-07-10 21:37:42 +0530

    Update project

--- views/component/bannercomponent.js
+++ views/component/bannercomponent.js
@@ -1,22 +1,85 @@
+// function Banner() {
+
+// return `
+
+// <div class="banner">
+
+//     <a href="/services">
+
+//         <img
+//             src="/uploads/banner.jpg"
+//             alt="Banner"
+//         >
+
+//     </a>
+
+// </div>
+
+// `;
+
+// }
+
+// module.exports = Banner;
+
+
 function Banner() {
 
 return `
 
-<div class="banner">
+<section class="hero-banner">
 
-    <a href="/services">
+    <div class="banner-overlay"></div>
 
-        <img
-            src="/uploads/banner.jpg"
-            alt="Banner"
-        >
+    <img
+        src="/uploads/banner.jpg"
+        alt="HHGS Banner"
+        class="banner-img"
+    >
 
-    </a>
+    <div class="banner-content">
 
-</div>
+        <span class="tag">
+            <i class="fa-solid fa-heart-pulse"></i>
+            Trusted Healthcare Platform
+        </span>
+
+        <h1>
+            Human Healthy <br>
+            Global Services
+        </h1>
+
+        <p>
+
+            Best Healthcare • Digital Services •
+            Professional Doctors • Student Training
+
+        </p>
+
+        <div class="banner-btns">
+
+            <a href="/student-login" class="btn-primary">
+
+                <i class="fa-solid fa-user-graduate"></i>
+
+                Student Login
+
+            </a>
+
+            <a href="/services" class="btn-secondary">
+
+                <i class="fa-solid fa-stethoscope"></i>
+
+                Explore Services
+
+            </a>
+
+        </div>
+
+    </div>
+
+</section>
 
 `;
-
 }
 
 module.exports = Banner;
