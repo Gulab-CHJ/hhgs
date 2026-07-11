a0bde15f7ad62f38dfbd1e2e01ab65fdefc9d4e9
gulab <mdg122112@gmail.com>
2026-07-11 20:29:01 +0530

    Update project

--- pages/Homepages.js
+++ pages/Homepages.js
@@ -11,6 +11,7 @@ function Home({ banner, doctors, services, students }) {
 <!DOCTYPE html>
 <html>
 <head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
 </head>
 <body>
