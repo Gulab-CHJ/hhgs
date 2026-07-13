ed3482261d0a607022f13bb0c1e5d65418220d09
gulab <mdg122112@gmail.com>
2026-07-13 08:45:58 +0530

    Update project

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const Home = require("./pages/Homepages");
 const express = require("express");
 const app = express();
+require("./config/database");
 
 // Body Parser
 app.use(express.urlencoded({ extended: true }));
