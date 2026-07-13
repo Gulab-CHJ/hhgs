b270cebb60813728582052c5fd6d7d0f26f6d5ad
gulab <mdg122112@gmail.com>
2026-07-13 08:23:11 +0530

    Update project

--- routes/admin.js
+++ routes/admin.js
@@ -6,4 +6,12 @@ router.get("/", (req, res) => {
     res.send(AdminLogin());
 });
 
+// Login submit
+router.post("/login", (req, res) => {
+    const { email, password } = req.body;
+
+    console.log(email, password);
+
+    res.send("Login Success");
+});
 module.exports = router;
