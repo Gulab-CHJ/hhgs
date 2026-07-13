0f29f23aee817d6a43683805e17d93257b7eeb2c
gulab <mdg122112@gmail.com>
2026-07-13 06:43:46 +0530

    Update project

--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const router = express.Router();
+// const express = require("express");
+// const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.send("Admin Page");
-});
+// router.get("/", (req, res) => {
+//     res.send("Admin Page");
+// });
 
-module.exports = router;
+// module.exports = router;
