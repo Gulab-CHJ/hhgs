e3228244ecf8931cfc808d80772e5e8ede5665ee
gulab <mdg122112@gmail.com>
2026-07-18 07:55:09 +0530

    Update project

--- routes/admin.js
+++ routes/admin.js
@@ -22,7 +22,7 @@ router.get("/admindashboard", (req, res) => {
 // router.get("/manage-banners", AdminController.manageBanners);
 // router.get("/add-banner", adminController.addBannerPage);
 // router.post("/add-banner", adminController.saveBanner);
-router.get("/manage-banner", AdminController.manageBanners);
+router.get("/manage-banners", AdminController.manageBanners);
 
 router.get("/add-banner", AdminController.addBannerPage);
 
