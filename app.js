9eb7d6e9ed9ba94a25078708f648f4ec1b606d5c
gulab <mdg122112@gmail.com>
2026-07-22 06:44:26 +0530

    update banner

--- app.js
+++ app.js
@@ -32,18 +32,56 @@
 //     console.log(`Server Running: http://localhost:${PORT}`);
 // });
 
+// const express = require("express");
+// const app = express();
+
+// require("./config/database");
+
+// // Body Parser
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+// // Static
+// app.use("/storage", express.static("storage"));
+// app.use(express.static("public"));
+
+
+// // Home Route
+// const indexRoutes = require("./routes/index");
+// app.use("/", indexRoutes);
+
+
+// // Admin Route
+// const adminRoutes = require("./routes/admin");
+// app.use("/admin", adminRoutes);
+
+
+
+
+
+// // Start Server
+// const PORT = 3000;
+
+// app.listen(PORT, () => {
+//     console.log(`Server Running: http://localhost:${PORT}`);
+// });
 const express = require("express");
+const path = require("path");
+
 const app = express();
 
 require("./config/database");
+
 
 // Body Parser
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Static
-app.use("/storage", express.static("storage"));
-app.use(express.static("public"));
+
+// Static Files
+app.use("/storage", express.static(path.join(__dirname, "storage")));
+
+app.use(express.static(path.join(__dirname, "public")));
 
 
 // Home Route
