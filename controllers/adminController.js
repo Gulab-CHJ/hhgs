d222fb0e5288cec0dc3204743987b7e23aad43b2
gulab <mdg122112@gmail.com>
2026-07-23 06:41:51 +0530

    update banner

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,64 +1,193 @@
+// const ManageBanners = require("../pages/ManageBanner");
+// const AddBanner = require("../pages/addpages/addBanner");
+// const Banner = require("../models/Banner");
+
+// // Add Banner Page
+// exports.addBannerPage = (req, res) => {
+//     res.send(AddBanner());
+// };
+
+// // Save Banner
+// exports.saveBanner = async (req, res) => {
+//     try {
+
+//         if (!req.file) {
+//             return res.status(400).send("Please upload a banner image.");
+//         }
+
+//         const banner = new Banner({
+//             title: req.body.title,
+//             image: "/storage/uploads/" + req.file.filename,
+//             link: req.body.link,
+//             active: true
+//         });
+
+//         await banner.save();
+
+//         res.redirect("/admin/manage-banner");
+
+//     } catch (err) {
+//         console.log(err);
+//         res.status(500).send(err.message);
+//     }
+// };
+
+// // Manage Banner Page
+// exports.manageBanners = async (req, res) => {
+//     try {
+
+//         const banners = await Banner.find().sort({ createdAt: -1 });
+
+//         res.send(ManageBanners(banners));
+
+//     } catch (err) {
+//         console.log(err);
+//         res.status(500).send("Error Loading Banners");
+//     }
+// };
+
+
+// exports.deleteBanner = async (req, res) => {
+
+//     try {
+
+//         await Banner.findByIdAndDelete(req.params.id);
+
+//         res.redirect("/admin/manage-banner");
+
+//     } catch (err) {
+
+//         console.log(err);
+//         res.status(500).send("Delete Banner Failed");
+
+//     }
+
+// };
+
 const ManageBanners = require("../pages/ManageBanner");
 const AddBanner = require("../pages/addpages/addBanner");
 const Banner = require("../models/Banner");
+
 
 // Add Banner Page
 exports.addBannerPage = (req, res) => {
     res.send(AddBanner());
 };
 
+
 // Save Banner
 exports.saveBanner = async (req, res) => {
+
     try {
 
+
         if (!req.file) {
-            return res.status(400).send("Please upload a banner image.");
+
+            return res.status(400)
+            .send("Please upload a banner image.");
+
         }
 
+
+
         const banner = new Banner({
+
+
             title: req.body.title,
-            image: "/storage/uploads/" + req.file.filename,
+
+
+            // Cloudinary Image URL
+            image: req.file.path,
+
+
             link: req.body.link,
+
+
             active: true
+
+
         });
+
+
 
         await banner.save();
 
+
+
         res.redirect("/admin/manage-banner");
 
+
+
     } catch (err) {
-        console.log(err);
-        res.status(500).send(err.message);
+
+
+        console.log("SAVE BANNER ERROR:", err);
+
+
+        res.status(500)
+        .send(err.message);
+
+
     }
+
 };
+
+
 
 // Manage Banner Page
 exports.manageBanners = async (req, res) => {
+
     try {
 
-        const banners = await Banner.find().sort({ createdAt: -1 });
+
+        const banners = await Banner
+        .find()
+        .sort({ createdAt: -1 });
+
+
 
         res.send(ManageBanners(banners));
 
+
+
     } catch (err) {
+
+
         console.log(err);
-        res.status(500).send("Error Loading Banners");
+
+
+        res.status(500)
+        .send("Error Loading Banners");
+
+
     }
+
 };
 
 
+
+// Delete Banner
 exports.deleteBanner = async (req, res) => {
 
     try {
 
+
         await Banner.findByIdAndDelete(req.params.id);
+
 
         res.redirect("/admin/manage-banner");
 
+
+
     } catch (err) {
 
+
         console.log(err);
-        res.status(500).send("Delete Banner Failed");
+
+
+        res.status(500)
+        .send("Delete Banner Failed");
+
 
     }
 
