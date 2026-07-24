cd223d729b7c12f6945bf9df8bbc02c75f8ec5cf
gulab <mdg122112@gmail.com>
2026-07-24 11:25:28 +0530

    doctor card click update

--- routes/admin.js
+++ routes/admin.js
@@ -565,29 +565,29 @@ router.get("/delete-product/:id", async (req, res) => {
 // const DoctorProfile = require("../pages/doctorProfile");
 
 
-router.get("/doctor/:id", async (req,res)=>{
+// router.get("/doctor/:id", async (req,res)=>{
 
-    try{
+//     try{
 
-        const doctor = await Doctor.findById(req.params.id);
-
-
-        if(!doctor){
-            return res.send("Doctor Not Found");
-        }
+//         const doctor = await Doctor.findById(req.params.id);
 
 
-        res.send(DoctorProfile(doctor));
+//         if(!doctor){
+//             return res.send("Doctor Not Found");
+//         }
 
 
-    }catch(err){
+//         res.send(DoctorProfile(doctor));
 
-        console.log(err);
 
-        res.status(500).send(err.message);
+//     }catch(err){
 
-    }
+//         console.log(err);
 
-});
+//         res.status(500).send(err.message);
+
+//     }
+
+// });
 
 module.exports = router;
