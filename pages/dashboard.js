70021193a605b0df0742278f6ab87d6a8e699014
gulab <mdg122112@gmail.com>
2026-07-25 06:54:34 +0530

    Update project

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,66 +1,10 @@
-// // const DashboardHeader = require("../views/component/dashboard/dashboardHeader");
-// // const DashboardSidebar = require("../views/component/dashboard/dashboardSidebar");
-// // const DashboardSearch = require("../views/component/dashboard/dashboardSearch");
-// // const DashboardStats = require("../views/component/dashboard/dashboardStats");
-// // const DashboardSection = require("../views/component/dashboard/dashboardSection");
-
-// // function AdminDashboard(data = {}) {
-
-// // return `
-// // <!DOCTYPE html>
-// // <html lang="en">
-
-// // <head>
-
-// // <meta charset="UTF-8">
-// // <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-// // <title>HHGS Admin Dashboard</title>
-
-// // <link rel="stylesheet" href="/css/admin-dashboard.css">
-// // <link rel="stylesheet" href="/css/dashboard-card.css">
-// // <link rel="stylesheet" href="/css/dashboard-responsive.css">
-
-// // <link rel="stylesheet"
-// // href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
-
-// // </head>
-
-// // <body>
-
-// // ${DashboardHeader()}
-
-// // <div class="admin-layout">
-
-// //     ${DashboardSidebar()}
-
-// //     <main class="admin-content">
-
-// //         ${DashboardSearch()}
-
-// //         ${DashboardStats(data)}
-
-// //         ${DashboardSection()}
-
-// //     </main>
-
-// // </div>
-
-// // <script src="/js/admin-dashboard.js"></script>
-
-// // </body>
-
-// // </html>
-// // `;
-
-// // }
-
-// // module.exports = AdminDashboard;
-
+// const DashboardHeader = require("../views/component/dashboard/dashboardHeader");
 // const DashboardSidebar = require("../views/component/dashboard/dashboardSidebar");
+// const DashboardSearch = require("../views/component/dashboard/dashboardSearch");
+// const DashboardStats = require("../views/component/dashboard/dashboardStats");
 // const DashboardSection = require("../views/component/dashboard/dashboardSection");
 
-// function AdminDashboard() {
+// function AdminDashboard(data = {}) {
 
 // return `
 // <!DOCTYPE html>
@@ -71,9 +15,11 @@
 // <meta charset="UTF-8">
 // <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-// <title>HHGS Admin</title>
+// <title>HHGS Admin Dashboard</title>
 
-
+// <link rel="stylesheet" href="/css/admin-dashboard.css">
+// <link rel="stylesheet" href="/css/dashboard-card.css">
+// <link rel="stylesheet" href="/css/dashboard-responsive.css">
 
 // <link rel="stylesheet"
 // href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
@@ -82,11 +28,17 @@
 
 // <body>
 
+// ${DashboardHeader()}
+
 // <div class="admin-layout">
 
 //     ${DashboardSidebar()}
 
 //     <main class="admin-content">
+
+//         ${DashboardSearch()}
+
+//         ${DashboardStats(data)}
 
 //         ${DashboardSection()}
 
@@ -104,6 +56,54 @@
 // }
 
 // module.exports = AdminDashboard;
+
+const DashboardSidebar = require("../views/component/dashboard/dashboardSidebar");
+const DashboardSection = require("../views/component/dashboard/dashboardSection");
+
+function AdminDashboard() {
+
+return `
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<title>HHGS Admin</title>
+
+
+
+<link rel="stylesheet"
+href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+
+</head>
+
+<body>
+
+<div class="admin-layout">
+
+    ${DashboardSidebar()}
+
+    <main class="admin-content">
+
+        ${DashboardSection()}
+
+    </main>
+
+</div>
+
+<script src="/js/admin-dashboard.js"></script>
+
+</body>
+
+</html>
+`;
+
+}
+
+module.exports = AdminDashboard;
 
 
 
